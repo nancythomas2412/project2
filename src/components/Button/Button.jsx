dafdb4e729c714cbc76a758c70223d07793af1f8
Nancy Thomas <nancytreasathomas@gmail.com>
2024-04-14 15:59:41 +0530

    second commit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,14 @@
-import { MdMessage } from "react-icons/md";
 
-const Button = () => {
+import styles from './Button.module.css';
+const Button = (props) => {
+    const {isOutline, text, icon, ...rest} = props;
+    // console.log(props);
   return (
-    <div>
-        <MdMessage /> Button
-    </div>
-  )
-}
+    <button {...rest} className={isOutline ? styles.outline_btn : styles.primary_btn}>
+        {icon}
+        {text}
+    </button>
+  );
+};
 
 export default Button;
